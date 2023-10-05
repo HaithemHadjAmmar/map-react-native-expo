871322dae9f151698db7f879aac14976d664b586
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2023-10-05 14:23:56 +0100

    Maps-react-native-expo

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
+  const [mapLat, setMapLat] = useState(36.8065); // Updated latitude for Tunisia
+  const [mapLong, setMapLong] = useState(10.1815); // Updated longitude for Tunisia
+
+  const locationData = [
+    { latitude: 36.8065, longitude: 10.1815 }, // Coordinates for Tunisia
+    { latitude: 6.84076664, longitude: 79.871323 },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: mapLat,
+          longitude: mapLong,
+          latitudeDelta: 5, // Adjust the delta values as needed
+          longitudeDelta: 5,
+        }}
+      >
+        {locationData.map((data, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: data.latitude,
+              longitude: data.longitude,
+            }}
+            title={`Marker ${index + 1}`}
+            description={`Weight: ${data.weight}`}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
@@ -13,8 +41,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
